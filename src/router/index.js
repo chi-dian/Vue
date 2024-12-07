9000d1d7386af6f81f41f08b456b2de5f2462f5b
Yang2377 <2964527911@qq.com>
2024-12-07 12:45:33 +0800

    second

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import adClazzify from '@/view/admin/adClazzify.vue'; // 确保路径正确
+
+// 定义路由
+const routes = [
+  {
+    path: '/test',
+    component: adClazzify,
+    hidden: true,
+  },
+  // ...其他路由
+];
+
+// 创建路由实例
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL), // 使用 import.meta.env
+  routes,
+});
+
+export default router;
---
+++ src/view/admin/adminLogin.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="adminLogin">
+    <el-card style="max-width: 480px">
+        <template #header>
+          <div class="card-heard">
+            <span>管理员登录页面</span>
+          </div>
+        </template>
+    </el-card>
+  </div>
+</template>
